760ced42c3461b6eee7133eefcb2ed859edcebdc
UdayPandey01 <pandeyuday113@gmail.com>
2025-09-23 16:34:12 +0530

    small fix

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,6 +1,8 @@
 import {Module} from '@nestjs/common';
 import {AuthModule} from './auth/auth.module';
 import { PrismaModule } from './core/database/prisma.module';
+import { ProblemModule } from './problems/problems.module';
+import { KafkaModule } from './kafka/kafka.module';
 
 @Module({
     imports: [
